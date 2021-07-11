a0d910f7029925c5dc5b17703921c24489408d8f
pcabezas19 <pedrocabezas03@gmail.com>
2021-07-11 14:07:23 -0300

    Ejemplo de ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import time
+driver = webdriver.Chrome(executable_path = r"C:\dchrome\chromedriver.exe")
+
+# Navega a la URL
+driver.get("http://www.google.com")
+time.sleep(3)
+driver.find_element_by_name("q").send_keys("Hola")
